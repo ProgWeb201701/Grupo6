b558134deba3f6606d49eec26f1b6136191c6ca1
Bruno Oliveira <oliveirabruno5046@gmail.com>
2017-06-28 19:11:56 -0300

    cadastro de alunos

--- Index/controller/cadastrarAluno.php
+++ Index/controller/cadastrarAluno.php
@@ -1,8 +1,6 @@
 <?php
-include '../Model/BD/ConexaoBanco.php';
+include '.';
 ini_set('display_errors', 1);
-
-
 
 
     $conexao = new ConexaoBanco();
@@ -11,6 +9,7 @@ ini_set('display_errors', 1);
     $matricula = $_POST['matriculaAluno'];
     $email = $_POST['emailAluno'];
     
+    $query = "INSERT INTO aluno (nomeAluno, senhaAluno, matriculaAluno, emailAluno) VALUES ('$nome','$senha','$matricula','$email');";
     
+    $conexao->$requisoesBanco($query);
 
-?>
