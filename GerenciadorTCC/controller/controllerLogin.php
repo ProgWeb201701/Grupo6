39f89778d85f8a821e91f98ce799409d9d9c0f38
Bruno Oliveira <oliveirabruno5046@gmail.com>
2017-06-30 02:26:02 -0300

    login

--- GerenciadorTCC/controller/controllerLogin.php
+++ GerenciadorTCC/controller/controllerLogin.php
@@ -1,10 +1,7 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include '../model/dados/conexaoBanco.php';
+ini_set('display_errors', 1);
 session_start();
 
 /**
@@ -19,16 +16,21 @@ session_start();
         $usuario = $_POST['usuarioLogin'];
         $senha = $_POST['senhaLogin'];
 
-        $result = mysql_query("SELECT * FROM aluno WHERE nomeAluno = '$usuario' AND senhaAluno = '$senha'");
+        $result = mysql_query("SELECT * FROM aluno WHERE nomeAluno = '$usuario' AND senhaAluno = '$senha';");
 
-        if (mysql_num_rows($result) > 0) {
-//            $nome = $_POST['nomeAluno'];
-//            $senha = $_POST['senhaAluno'];
-            header("Location: ../view/home_aluno.php");
-        } else {
-            $lc = new controllerLogin();
-            $lc->loginProfessor();
+        if (!$result) {
+            echo 'aaaaaaaaa';
         }
+        
+        
+//        if (mysql_num_rows($result) > 0) {
+////            $nome = $_POST['nomeAluno'];
+////            $senha = $_POST['senhaAluno'];
+//            header("Location: ../view/home_aluno.php");
+//        } else {
+//            $lc = new controllerLogin();
+//            $lc->loginProfessor();
+//        }
 //    }
 
 //    function loginProfessor() {
